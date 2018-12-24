4e3fd91716adad6b8059f7a71203b617fb0249d7
David Yan <daveddd97@hotmail.com>
2018-12-25 00:35:01 +0100

    design complete

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <div class="container">
         <input type="text" placeholder="Födelsenummer">
-        <button type="submit" onclick="addHoroscope()">Spara mitt horoskop</button>
-        <button type="submit" onclick="updateHoroscope()">Uppdatera mitt horoskop</button>
-        <button type="submit" onclick="deleteHoroscope()">Radera mitt horoskop</button>
+        <button class="save" type="submit" onclick="addHoroscope()">Spara mitt horoskop</button>
+        <button class="update" type="submit" onclick="updateHoroscope()">Uppdatera mitt horoskop</button>
+        <button class="delete" type="submit" onclick="deleteHoroscope()">Radera mitt horoskop</button>
     </div>
 </body>
 </html>
